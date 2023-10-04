e125efa7e3be8b7fb33b484ec2a385d45afef969
Bodgar Jair Espinosa Miranda <bodgar_jair@hotmail.com>
2023-10-04 17:32:18 -0600

    Initial Commit

---
+++ TranslateDataManager/TranslateDataManager.h
@@ -0,0 +1,18 @@
+//
+//  TranslateDataManager.h
+//  TranslateDataManager
+//
+//  Created by Bodgar Espinosa Miranda on 04/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TranslateDataManager.
+FOUNDATION_EXPORT double TranslateDataManagerVersionNumber;
+
+//! Project version string for TranslateDataManager.
+FOUNDATION_EXPORT const unsigned char TranslateDataManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TranslateDataManager/PublicHeader.h>
+
+
